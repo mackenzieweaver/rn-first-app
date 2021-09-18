ec38e28267297306735b6b602049cc0adee2fcb2
Mack <macknz7@gmail.com>
2021-09-18 15:37:14 -0400

    goalsapp

--- App.js
+++ App.js
@@ -1,21 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, TextInput, Button, StyleSheet, Text, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [enteredGoal, setEnteredGoal] = React.useState('');
+	const [GOALS, setGoals] = React.useState([]);
+
+	const RenderItem = ({key, value}) => (
+		<View key={key} style={styles.listItem}>
+			<Text>{value}</Text>
+		</View>
+	);
+
+	return (
+		<View style={styles.screen}>
+
+			<View style={styles.inputContainer}>
+				<TextInput
+					style={styles.input}
+					onChangeText={setEnteredGoal}
+					value={enteredGoal}
+				/>
+				<Button
+					onPress={() => {
+						setGoals([...GOALS, {id: Math.random().toString(), value: enteredGoal}]);
+					}}
+					title="Add"
+				/>
+			</View>
+
+			<FlatList
+				keyExtractor={(item, index) => item.id}
+				data={GOALS}
+				renderItem={item => RenderItem(item.item)}
+			/>
+
+		</View>
+	);
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	screen: {
+		padding: 50
+	},
+	inputContainer: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center'
+	},
+	input: {
+		borderWidth: 1,
+		width: '80%',
+		padding: 10
+	},
+	listItem: {
+		padding: 10,
+		marginVertical: 10,
+		backgroundColor: '#ccc',
+		borderColor: 'black',
+		borderWidth: 1
+	}
 });
