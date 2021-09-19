313d9392ac7c0c982217a9ec92709548d0dce8b6
Mack <macknz7@gmail.com>
2021-09-19 17:29:55 -0400

    moved "add goal" input into a modal

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, FlatList } from 'react-native';
+import { View, StyleSheet, FlatList, Button } from 'react-native';
 import GoalItem from './components/GoalItem';
 import GoalInput from './components/GoalInput';
 
 export default function App() {
 	const [GOALS, setGoals] = useState([]);
+	const [showAddGoalModal, setShowAddGoalModal] = useState(false);
 	return (
 		<View style={styles.screen}>
-
-			<GoalInput setGoals={setGoals} goals={GOALS} />
+			<Button title="Add Goal" onPress={() => setShowAddGoalModal(true)} />
+			<GoalInput visible={showAddGoalModal} setShowAddGoalModal={setShowAddGoalModal} setGoals={setGoals} goals={GOALS} />
 
 			<FlatList
 				keyExtractor={item => item.id}
--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,41 +1,43 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet } from 'react-native';
+import { View, TextInput, Button, StyleSheet, Modal } from 'react-native';
 
-const GoalInput = ({setGoals, goals}) => {
+const GoalInput = ({ setGoals, goals, visible, setShowAddGoalModal }) => {
     const [enteredGoal, setEnteredGoal] = useState('');
     return (
-        <View style={styles.inputContainer}>
-
-
-            <TextInput
-                style={styles.input}
-                onChangeText={setEnteredGoal}
-                value={enteredGoal}
-            />
-            <Button
-                onPress={() => {
-                    setGoals([...goals, 
-                        { 
-                            id: Math.random().toString(),
-                            value: enteredGoal 
-                        }
-                    ]);
-                }}
-                title="Add"
-            />
-
-        </View>
+        <Modal visible={visible} animationType="slide">
+            <View style={styles.inputContainer}>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={setEnteredGoal}
+                    value={enteredGoal}
+                />
+                <View style={{flexDirection: "row"}}>
+                <Button
+                    onPress={() => {
+                        setGoals([...goals,
+                            {
+                                id: Math.random().toString(),
+                                value: enteredGoal
+                            }
+                        ]);
+                    }}
+                    title="Add"
+                />
+                <Button title="Close" onPress={() => setShowAddGoalModal(false)} />
+                </View>
+            </View>
+        </Modal>
     );
 }
 
 export default GoalInput;
 
-const styles = StyleSheet.create({    
-	inputContainer: {
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-		alignItems: 'center'
-	},
+const styles = StyleSheet.create({
+    inputContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
     input: {
         borderWidth: 1,
         width: '80%',
